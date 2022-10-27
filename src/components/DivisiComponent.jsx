43e4cc374cb36d93299460767ef5b865b4ffbeec
Muhaziz28 <muhammadhaikalaziz28@gmail.com>
2022-10-27 16:29:58 +0700

    soraya frontend

--- src/components/DivisiComponent.jsx
+++ src/components/DivisiComponent.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Modal, Button } from "react-bootstrap";
 
 const DivisiComponent = () => {
   const user = useSelector((state) => state.auth.user);
   const [divisis, setDivisis] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const [deleteId, setDivisiId] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
 
   useEffect(() => {
     getDivisis();
@@ -16,6 +24,22 @@ const DivisiComponent = () => {
     setDivisis(response.data.payload.datas);
     setLoading(true);
   };
+
+  const handleDeleteDivisi = async (id) => {
+    setDivisiId(id);
+    // console.log(id);
+    setShow(true);
+  };
+  const handleDeleteDivisiItem = async () => {
+    await axios.delete(`http://localhost:5000/divisi/${deleteId}`);
+    setShow(false);
+    getDivisis();
+  };
+
+  // const deleteDivisi = async (divisiId) => {
+  //   await axios.delete(`http://localhost:5000/divisi/${divisiId}`);
+  //   getDivisis();
+  // };
 
   const result = divisis.map((divisi) => (
     <div className="col-6 col-md-4 col-lg-3" key={divisi.id}>
@@ -37,7 +61,12 @@ const DivisiComponent = () => {
           {user && user.payload.datas.role_id === 1 ? (
             <>
               <button className="btn text-warning rounded-md">Edit</button>
-              <button className="btn text-danger rounded-md">Delete</button>
+              <button
+                onClick={() => handleDeleteDivisi(divisi.id)}
+                className="btn text-danger rounded-md"
+              >
+                Delete
+              </button>
             </>
           ) : (
             ""
@@ -49,16 +78,45 @@ const DivisiComponent = () => {
 
   return (
     <div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Hapus Divisi</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Apakah anda yakin ingin menghapus divisi ini?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleDeleteDivisiItem}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="content">
         <div className="row">
           <div className="col-12">
-            <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between item-center">
               <div>
                 <h2 className="content-title">Divisi</h2>
                 <h5 className="content-desc mb-4">Your team powers</h5>
               </div>
-              <div></div>
-              <button className="btn btn-outline-primary">Add Divisi</button>
+              <div>
+                <Link
+                  className="btn btn-outline-info"
+                  style={{
+                    borderRadius: "20px",
+                    padding: "10px 20px",
+                  }}
+                  to="/divisi/add"
+                >
+                  Tambah Divisi
+                </Link>
+              </div>
             </div>
           </div>
         </div>
